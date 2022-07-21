553091d1efc9f1c5c965a090c6dec4d0cb9c1207
Hose <hose1021@gmail.com>
2022-07-21 12:15:14 +0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,323 @@
-<script setup>
-import { ref } from 'vue'
-
-let counter = ref(0)
-
-setInterval(() => {
-  counter.value++
-}, 1000)
-</script>
-
 <template>
-  <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1
-          @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
-        >
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
-  </div>
+    <div>
+        <header>
+            <div class="container mx-auto">
+                <div class="flex justify-between items-center py-3 xs:px-8">
+                    <div class="flex items-center space-x-6">
+                        <a href="/" class="text-lg font-bold">
+                            <span class="text-sky-600">H</span>ire
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="overflow-hidden relative mb-12">
+            <section class="space-y-52">
+                <div class="py-32 text-center">
+                    <div class="space-y-6">
+                        <div class="z-50 font-nuni text-5xl font-extrabold leading-tight text-gray-900 md:text-4xl">
+                            Анонимный поиск работы на <span class="text-sky-600 dark:text-sky-500">H</span>ire
+                        </div>
+                        <div
+                            class="my-12 mx-auto w-[50%] max-w-3xl text-lg text-center text-slate-600 xs:w-[90%]">
+                            Вы пишите о своём опыте, ожидания и желания по зарплате, а
+                            <span class="font-medium text-sky-500">компании предлагают вам вакансии</span>. Только <span
+                            class="font-medium text-sky-500">вы принимаете решение</span> кому показывать свои
+                            контактные данные.
+                        </div>
+                        <div
+                            class="flex items-center text-white mx-auto w-max h-10 text-sm font-bold text-gray-800 border rounded px-4">
+                            Скоро
+                        </div>
+                    </div>
+                    <div
+                        class="before:top-[4%] before:left-[20%] before:w-[15%] before:h-[6.9%] text-xl xs:hidden dots"
+                    />
+                    <div
+                        class="before:top-[10%] before:left-[60%] before:w-[15%] before:h-[5.9%] text-xl xs:hidden dots"
+                    />
+                </div>
+                <section class="container mx-auto space-y-16 xs:px-8">
+                    <div class="space-y-12">
+                        <div class="space-y-2 text-center">
+                            <div class="font-nuni text-3xl font-semibold text-sky-600">
+                                Чем Hire отличается от LinkedIn?
+                            </div>
+                        </div>
+                        <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+                            <div class="space-y-2">
+                                <h1
+                                    class="font-nuni text-sm font-semibold leading-6 text-gray-800 lg:text-lg">
+                                    <i class="iconify text-xl text-sky-600 align-[-0.12rem]"
+                                       data-icon="tabler:face-id-error"></i>
+                                    Анонимность
+                                </h1>
+                                <p class="mt-2 text-gray-500 dark:text-gray-400">
+                                    Вы анонимны для всех кроме тех с кем вы делитесь своими контактами.
+                                </p>
+                            </div>
+                            <div class="space-y-2">
+                                <h1
+                                    class="font-nuni text-sm font-semibold leading-6 text-gray-800 lg:text-lg">
+                                    <i class="iconify text-xl text-sky-600 align-[-0.12rem]"
+                                       data-icon="mdi:run-fast"></i>
+                                    Быстрый старт
+                                </h1>
+                                <p class="mt-2 text-gray-500 dark:text-gray-400">
+                                    В два клика регистрация и поиск работы.
+                                </p>
+                            </div>
+                            <div class="space-y-2">
+                                <h1 class="font-nuni text-sm font-semibold leading-6 text-gray-800 lg:text-lg">
+                                    <i class="iconify text-xl text-sky-600 align-[-0.12rem]"
+                                       data-icon="tabler:search"></i>
+                                    Актуальный поиск
+                                </h1>
+                                <p class="mt-2 text-gray-500 dark:text-gray-400">
+                                    Только у нас можно узнать, реально ли человек ищет работу.
+                                </p>
+                            </div>
+                            <div class="space-y-2">
+                                <h1 class="font-nuni text-sm font-semibold leading-6 text-gray-800 lg:text-lg xl:text-xl">
+                                    <i class="iconify text-xl text-sky-600 align-[-0.12rem]"
+                                       data-icon="tabler:bolt"></i>
+                                    Актуальная база
+                                </h1>
+                                <p class="mt-2 text-gray-500 dark:text-gray-400">
+                                    На Hire нет "мертвых душ".
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div class="flex items-center -mx-52 xs:px-8">
+                        <div class="w-1/2 text-center space-y-5 whitespace-nowrap">
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Tikinti
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Tibb
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Satış
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Maliyyə
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    İnzibati
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Marketing
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Hüquqşünaslıq
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Dizayner
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Təhsil
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    İT
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Media
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Memar
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Mühəndis
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Prorab
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Həkim
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Tibb
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Sanitar
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Rieltor
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Satış
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Ekspeditor
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Mercendayzer
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Maliyyə
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Auditor
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Mühasib
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Anbardar
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Kassir
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Kredit
+                                </div>
+                            </div>
+                        </div>
+                        <div class="space-y-4 w-1/2 mx-auto text-center">
+                            <div class="font-nuni text-3xl font-semibold text-sky-600 text-center">
+                                На Hire большое разнообразие профессий
+                            </div>
+                            <div
+                                class="mx-auto w-[67%] text-lg text-center text-slate-600 xs:w-[80%] xs:text-md">
+                                Вы с легкостью заполнив информацию о себе, сможете найти подходящую себе работу по вашей
+                                профессии. Hire подсветит вам вакансию если она близка вам по профессии и по желаниям
+                            </div>
+                        </div>
+                        <div class="w-1/2 text-center space-y-5 whitespace-nowrap">
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Sığorta
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Maliyyə
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Biznes
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Maliyyə
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    İnzibati
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    İqtisadçı
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Administrator
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Assistent
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Ofis
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Resepshn
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Sosial
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    İctimai
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Promouter
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Kopirayter
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Vəkil
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Notarius
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Hüquqşünas
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Təlimçi
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Müəllim
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Frontend
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Backend
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Mobile
+                                </div>
+                            </div>
+                            <div class="space-x-4">
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Desktop
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Videograf
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Fotograf
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Jurnalist
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Operator
+                                </div>
+                                <div class="inline-block bg-gray-100 rounded px-2.5 py-1.5 w-max">
+                                    Poligrafiya
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="container mx-auto space-y-16 xs:px-8">
+                    <div class="space-y-8">
+                        <div class="space-y-2 text-center">
+                            <div class="font-nuni text-3xl font-semibold text-sky-600">
+                                Зачем нужен «еще один» работный сайт?
+                            </div>
+                            <div class="text-sm">
+                                Мы не хотим делать "еще один сайт", мы хотим поменять весь подход к поиску
+                                кандидатов.
+                            </div>
+                        </div>
+                        <div class="text-center text-gray-500 text-xl w-3/4 mx-auto">
+                            Идея, что кандидаты должны "откликаться" на вакансии безнадежно устарела. Хороших
+                            кандидатов нужно искать. Мы делаем сервис, который "заточен" на кандидатов.
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </div>
+        <footer class="py-4 mt-auto w-full bg-white dark:bg-gray-800">
+            <div class="container flex justify-between items-center mx-auto">
+                <a href="#"
+                   class="text-xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300">Hire™</a>
+                © 2022 Made with ❤ in Azerbaijan
+            </div>
+        </footer>
+    </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,79 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
-      },
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        fontFamily: {
+            nuni: ['Nunito,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji'],
+            sans: ['inter', 'Roboto', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Apple Color Emoji', 'Arial, sans-serif', 'Segoe UI Emoji', 'Segoe UI Symbol']
+        },
+        screens: {
+            xs: { max: '480px' }, // Mobile (iPhone 3 - iPhone XS Max).
+            sm: { min: '480px', max: '768px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+            md: { min: '768px', max: '1024px' }, // Tablet (matches max: iPad Pro @ 1112px).
+            lg: { min: '1024px' }, // Desktop smallest.
+            xl: { min: '1020px' } // Desktop widescreen.
+        },
+        borderColor: theme => ({
+            ...theme('colors'),
+            DEFAULT: '#e9e9e9',
+            primary: '#3490dc',
+            secondary: '#ffed4a',
+            danger: '#e3342f'
+        }),
+        extend: {
+            width: {
+                left: '75%',
+                right: '25%'
+            },
+            borderColor: {
+                DEFAULT: '#e9e9e9'
+            },
+            colors: {
+                header: '#1a1a1ae6',
+                cerulean: {
+                    default: '#095977',
+                    100: '#0D84AF',
+                    200: '#0C79A1',
+                    300: '#0B6F93',
+                    400: '#0A6485',
+                    500: '#095c79',
+                    600: '#084F68',
+                    700: '#07445A',
+                    800: '#06394C',
+                    900: '#052E3E'
+                },
+                primaryBlueDark: '#0068b3',
+                primaryBlueHoverDark: '#1070b5',
+                graphlightblue: '#47b7f8',
+                graphlightgreen: '#19652a',
+                graphdarkgreen: '#40d763',
+                warning: '#efecc8',
+                warningDark: '#6e6a33',
+                redDark: 'rgb(54, 36, 34)',
+                primaryLight: '#202020',
+                error: '#efc8c8',
+                errorDark: '#6e3333',
+                primaryDark: '#1a1c1e',
+                secondaryDark: '#24282e',
+                tertiaryDark: '#3c414c',
+                selectedDark: '#434343',
+                hoverDark: 'rgba(255, 255, 255, 0.055)',
+                secondaryLight: '#737373',
+                divider: '#262626',
+                dividerLight: '#1f1f1f',
+                dividerDark: '#404449',
+                input: '#040404'
+            },
+            animation: {
+                'ping-slow': 'ping 2.5s cubic-bezier(1, 1, 0.2, 1) infinite'
+            }
+        }
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio')
+    ]
+};
